e6d74b6d4a4fca997b8316ec220e581ad71d1a8a
iamshammas <muhdshammas612@gmail.com>
2026-07-23 19:07:19 +0530

    Add viewsets for Category, Brand, Unit, and Product models

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product, Category, Brand, Unit
+from .serializers import ProductSerializer, CategorySerializer, BrandSerializer, UnitSerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class BrandViewSet(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+class UnitViewSet(viewsets.ModelViewSet):
+    queryset = Unit.objects.all()
+    serializer_class = UnitSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
